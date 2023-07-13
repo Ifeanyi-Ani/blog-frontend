bb25530ed55ecee962b3b3a21175db7c4331d5e5
Ifeanyi Ani <ifeanyiani155@gmail.com>
2023-07-13 17:40:18 +0100

    displaying the the comment legnth per post and add delete funtion to it

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -8,6 +8,7 @@ import {
   createComment,
   deleteComment,
 } from "../redux/comments/comment.actions";
+import baseUrl from "../apis/baseUrl";
 
 type PostCardProps = {
   userId: string;
@@ -22,6 +23,7 @@ type PostCardProps = {
   createComment: any;
   comments: any;
   fetchComments: any;
+  deleteComment: any;
   currentUser: any;
   children?: ReactNode;
 };
@@ -40,31 +42,44 @@ const PostCard = ({
   comments,
   currentUser,
   fetchComments,
+  deleteComment,
 }: PostCardProps) => {
+  const [check, setCheck] = useState<boolean>(false);
+
+  const [postComments, setPostComments] = useState();
   const init_data = {
     text: "",
     userId: currentUser?.data?.user?._id || "",
   };
   const [comment, setComment] = useState(init_data);
-  function handleCommentSubmit(postId) {
+  async function handleCommentSubmit(postId) {
     const updatedComment = { ...comment, postId };
-    console.log(updatedComment, postId);
-    createComment(postId, updatedComment);
+    await createComment(postId, updatedComment);
+    await fetchComments(postId);
     setComment(init_data);
   }
   function handleComment(postId) {
     fetchComments(postId);
+
+    setCheck(!check);
   }
 
+  async function getAllComments() {
+    const response = await baseUrl.get("/posts/comments");
+    const allCommets = await response.data.data.comments;
+    setPostComments(allCommets);
+    console.log("hi");
+  }
+  async function handleDeleteComment(postId, id) {
+    if (confirm("Are you sure you want to delete this comment?")) {
+      await deleteComment(postId, id);
+      await fetchComments(postId);
+      getAllComments();
+    }
+  }
   useEffect(() => {
-    fetchComments(postId);
-  }, [fetchComments, postId]);
-
-  // Get the comments count for the current post
-  // const postComments = comments?.comments?.filter(
-  //   comment => comment.postId === postId
-  // );
-  // // const commentsCount = postComments.length;z
+    getAllComments();
+  }, [postId]);
 
   return (
     <div>
@@ -84,13 +99,28 @@ const PostCard = ({
           </Card.Text>
         </Card.Body>
         <Card.Footer style={{ borderTop: "none" }} className='d-flex'>
-          <div
-            className='border rounded-5 d-flex justify-content-center align-items-center p-2'
-            role='button'
-            onClick={() => handleComment(postId)}
-          >
-            {comments ? comments.results + " notes" : "0 notes"}
-          </div>
+          {currentUser ? (
+            <div
+              className='border rounded-5 d-flex justify-content-center align-items-center p-2'
+              role='button'
+              onClick={() => handleComment(postId)}
+            >
+              {postComments
+                ? postComments.filter(comment => comment.postId === postId)
+                    .length + " notes"
+                : "notes"}
+            </div>
+          ) : (
+            <div
+              className='border rounded-5 d-flex justify-content-center align-items-center p-2'
+              role='button'
+            >
+              {postComments
+                ? postComments.filter(comment => comment.postId === postId)
+                    .length + " notes"
+                : "notes"}
+            </div>
+          )}
           <Stack className='footer-img ms-auto gap-3' direction='horizontal'>
             <img src={shareLogo} alt='logo' role='button' />
             <img src={reloadLogo} alt='logo' role='button' />
@@ -98,7 +128,72 @@ const PostCard = ({
           </Stack>
         </Card.Footer>
 
-        {/* Rest of the component */}
+        {check && (
+          <div className='commentSec p-3'>
+            <div className='commentNav border-bottom-1 d-flex gap-3'>
+              <div>
+                <i className='bi bi-chat'></i>0
+              </div>
+              <div>
+                <i className='bi bi-fullscreen'></i>900
+              </div>
+              <div>
+                <i className='bi bi-heart'></i>90
+              </div>
+              <div>
+                <i className='bi bi-fullscreen'></i>900
+              </div>
+              <div className='ms-auto'>Comments</div>
+            </div>
+            <div className='commentsBody'>
+              <div>
+                <form action='' className='d-flex'>
+                  <textarea
+                    name=''
+                    id=''
+                    cols='30'
+                    rows='2'
+                    value={comment.text}
+                    onChange={e =>
+                      setComment({ ...comment, text: e.target.value })
+                    }
+                    style={{ width: "90%" }}
+                  />
+                  <button
+                    type='button'
+                    onClick={() => handleCommentSubmit(postId)}
+                  >
+                    send
+                  </button>
+                </form>
+              </div>
+              {comments?.data?.comments ? (
+                [...comments.data.comments].reverse().map(comment => (
+                  <div
+                    className='commentItem'
+                    style={{ borderBottom: "1px solid gainsboro" }}
+                    key={comment._id}
+                  >
+                    <div className='d-flex'>
+                      <Avater />
+                      {comment.userId.username}
+                    </div>
+                    <div>{comment.text}</div>
+                    {currentUser?.data?.user?._id === comment.userId._id ? (
+                      <i
+                        className='bi bi-trash-fill delBtn'
+                        role='button'
+                        onClick={() => handleDeleteComment(postId, comment._id)}
+                      ></i>
+                    ) : null}
+                  </div>
+                ))
+              ) : (
+                <div>Loading Comment</div>
+              )}
+            </div>
+          </div>
+        )}
       </Card>
     </div>
   );
@@ -111,6 +206,7 @@ const mapStateToProps = ({ user: { currentUser }, comment: { comments } }) => ({
 const mapDispatchToProps = {
   createComment,
   fetchComments,
+  deleteComment,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostCard);
