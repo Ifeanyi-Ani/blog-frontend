75b36cdf7febe02e58de86cc8aa931b6403d5d28
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-02-13 14:06:28 +0100

    fix: undefine id when re-rendering post preview fix #12

--- src/pages/Post-Preview.tsx
+++ src/pages/Post-Preview.tsx
@@ -1,18 +1,13 @@
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
-import PostCard from "../components/PostCard"
-import { useAppDispatch, useAppSelector } from "../app/hook"
-import { IPost } from "../types/type"
-import { getPosts } from "../features/posts/postSlice"
+import { useParams } from "react-router-dom";
+import PostCard from "../components/PostCard";
+import { useAppDispatch, useAppSelector } from "../app/hook";
 
 const PostPreview = () => {
-  const { id } = useParams();
-  const post = useAppSelector(state => state?.posts?.posts?.find(post => post.id === id))
-  return (
-    <PostCard
-      post={post!}
-    />
-  )
-}
+  const { id } = useParams<{ id: string }>();
+  const post = useAppSelector((state) =>
+    state?.posts?.posts?.find((post) => post?.id === id),
+  );
+  return post ? <PostCard post={post} /> : null;
+};
 
-export default PostPreview
+export default PostPreview;
