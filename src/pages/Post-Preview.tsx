244b67e1723f72ac74ef3190d2038a342efea2ff
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-04-17 16:05:49 +0100

    fix error post preview
    correct the error message for post preview

--- src/pages/Post-Preview.tsx
+++ src/pages/Post-Preview.tsx
@@ -1,15 +1,24 @@
 import { useParams } from "react-router-dom";
 import PostCard from "../components/PostCard";
 import { useGetPostQuery } from "../features/posts/postSlice";
+import toast from "react-hot-toast";
 
 const PostPreview = () => {
   const { id } = useParams<{ id: string }>();
   const { data: post, isLoading, error } = useGetPostQuery(id!);
   let content: JSX.Element;
-  if (isLoading) content = <div>Loading</div>;
-  else if (error)
-    content = <div>{error.data.message || "Something went wrong"}</div>;
-  else if (post) content = <PostCard post={post} />;
+  if (isLoading) {
+    content = <div>Loading</div>;
+  } else if (error) {
+    if ("data" in error!) {
+      const errorData = error as any;
+      const message =
+        errorData?.data.msg ||
+        errorData?.data?.message ||
+        "something went wrong";
+      toast.error(message);
+    }
+  } else if (post) content = <PostCard post={post} />;
 
   return content!;
 };
