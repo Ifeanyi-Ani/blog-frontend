553aac45fae20b5cc322a96bab204681d88b6b08
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-04-17 21:58:27 +0100

    use rtkq to handle remote state

--- src/features/users/userSlice.ts
+++ src/features/users/userSlice.ts
@@ -1,12 +1,9 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import API from "../../apis/baseUrl";
-import { RootState } from "../../app/store";
 import { IUser } from "../../types/type";
 import { apiSlice } from "../api/apiSlice";
-import { UserLogin } from "./authSlice";
+import { UserLogin, UserLogout } from "./authSlice";
 
 interface IUserData {
-  formData: Partial<IUser>;
+  formData: Partial<IUser | any>;
   id: string;
 }
 
@@ -62,11 +59,17 @@ const usersSlice = apiSlice.injectEndpoints({
       async onQueryStarted(_arg, { queryFulfilled, dispatch }) {
         try {
           await queryFulfilled;
-          dispatch(UserLogOut());
+          dispatch(UserLogout());
         } catch (error) {
           console.error(error);
         }
       },
+    }),
+
+    getUsers: builder.query({
+      query: () => ({
+        url: "users",
+      }),
     }),
 
     updateUser: builder.mutation({
@@ -112,113 +115,14 @@ const usersSlice = apiSlice.injectEndpoints({
     }),
   }),
 });
-interface UserState {
-  users?: IUser[];
-  status: "idle" | "loading" | "success" | "error";
-  error: string | null;
-}
-const initialState = {
-  users: [],
-  user: null,
-  currentUser: null,
-  status: "loading",
-  error: null,
-} as UserState;
-
-export const fetchUsers = createAsyncThunk<
-  IUser[],
-  void,
-  { rejectValue: string }
->("users/fetchUsers", async (_, thunkApi) => {
-  try {
-    const response = await API.get("/users");
-    const data = response.data as IUser[];
-    return data;
-  } catch (error) {
-    return thunkApi.rejectWithValue("failed to fetch issues");
-  }
-});
-
-export const createUser = createAsyncThunk<
-  IUser,
-  void,
-  { rejectValue: string }
->("users/createUser", async (formData, thunkApi) => {
-  try {
-    const response = await API.post("/users", formData);
-    const data = response.data as IUser;
-    return data;
-  } catch (error) {
-    return thunkApi.rejectWithValue("failed to create user");
-  }
-});
-
-export const login = createAsyncThunk(
-  "auth/login",
-  async (formData, thunkApi) => {
-    try {
-      const response = await API.post("/auth/login", formData);
-      const data = response.data;
-      return data;
-    } catch (error) {
-      return thunkApi.rejectWithValue("failed to loggedin User");
-    }
-  },
-);
-
-export const signup = createAsyncThunk(
-  "auth/signup",
-  async (formData, thunkApi) => {
-    try {
-      const response = await API.post("/auth/signup", formData);
-      const data = response.data;
-      return data;
-    } catch (error) {
-      return thunkApi.rejectWithValue("failed to signup User");
-    }
-  },
-);
-
-export const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchUsers.pending, (state: UserState) => {
-        state.status = "loading";
-      })
-      .addCase(fetchUsers.fulfilled, (state: UserState, action) => {
-        state.status = "success";
-        state.users = action.payload;
-      })
-      .addCase(fetchUsers.rejected, (state: UserState, action) => {
-        state.status = "error";
-        state.error = action.error.message || "Something went wrong";
-      })
-      .addCase(createUser.fulfilled, (state: UserState, action) => {
-        state?.users?.push(action.payload);
-      })
-      .addCase(login.fulfilled || signup.fulfilled, (_, action) => {
-        localStorage.setItem("currentUser", JSON.stringify(action.payload));
-      });
-  },
-});
 
 export const {
   useLoginMutation,
   useSignUpMutation,
   useLogOutMutation,
   useRefreshQuery,
-  useGetLoggedUserQuery,
+  useGetUsersQuery,
   useUpdateUserMutation,
   useDeleteUserMutation,
   useGetUserQuery,
 } = usersSlice;
-export const getUsers = (state: RootState) => state.users.users;
-export const getUser = (state: RootState, userId: string) =>
-  state?.users?.users?.find((user) => user.id === userId);
-export default userSlice.reducer;
-function UserLogOut(): any {
-  throw new Error("Function not implemented.");
-}
