c9dab2540b3ed6e3a138da9e22db3995798c468f
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-04-17 21:55:13 +0100

    fix store error based on the new changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
 
-import postSlice from "../features/posts/postSlice";
-import userSlice from "../features/users/userSlice";
-import commentSlice from "../features/comments/commentSlice";
 import authSlice from "../features/users/authSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
-    posts: postSlice,
-    users: userSlice,
-    comments: commentSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
@@ -21,9 +15,6 @@ export const store = configureStore({
 const initializeApp = async () => {
   await store.dispatch(
     apiSlice.endpoints.refresh.initiate({}, { forceRefetch: true }),
-  );
-  await store.dispatch(
-    apiSlice.endpoints.getLoggedUser.initiate({}, { forceRefetch: true }),
   );
 };
 
