517c4327178e31178abcbc909d6f19f0bc6e8b8a
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-09-22 23:41:13 +0100

    update: dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 import { Provider } from "react-redux";
-import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
+
+import App from "./App.tsx";
 import ContextProvider from "./contexts/contextData.tsx";
 import { store } from "./app/store.ts";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
