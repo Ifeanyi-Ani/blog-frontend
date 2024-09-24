dfdd76f0216b999de8db69f62b0382b56d03c3f3
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-09-24 16:38:48 +0100

    custom dropdown menu

---
+++ src/ui/shared/DropDownMenu.tsx
@@ -0,0 +1,25 @@
+import { Menu, MenuButton, Transition } from "@headlessui/react";
+import { MoreHorizontal } from "lucide-react";
+import { ReactNode } from "react";
+interface DropDownMenuProps {
+  children: ReactNode;
+}
+export const DropDownMenu: React.FC<DropDownMenuProps> = ({ children }) => {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <MenuButton className="text-electricCyan-400 hover:text-electricCyan-300 transition-colors duration-200">
+        <MoreHorizontal />
+      </MenuButton>
+      <Transition
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        {children}
+      </Transition>
+    </Menu>
+  );
+};
