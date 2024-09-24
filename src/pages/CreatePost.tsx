bc4dca49bf23fe428d108517d7dfa9251c5fec77
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-09-24 16:37:28 +0100

    create post form

---
+++ src/pages/CreatePost.tsx
@@ -0,0 +1,28 @@
+import { useAppSelector } from "../app/hook";
+import { CreatePostForm } from "../features/posts/CreatePostForm";
+import { Navigate, useNavigate } from "react-router-dom";
+import { BackBtn } from "../ui/shared/BackBtn";
+import { GlowHeading } from "../ui/shared/GlowHeading";
+
+const CreatePostPage = () => {
+  const { currentUser } = useAppSelector((state) => state.auth);
+
+  if (!currentUser) {
+    return <Navigate to="/auth/login" replace />;
+  }
+  return (
+    <>
+      <div className="mb-8">
+        <BackBtn text="Home" />
+      </div>
+
+      <div className="space-y-2 text-center">
+        <GlowHeading heading="Create New Post" />
+      </div>
+
+      <CreatePostForm />
+    </>
+  );
+};
+
+export default CreatePostPage;
