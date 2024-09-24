b7c92816f730d8681d41175e170f8f91b2e95a43
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-09-24 16:34:59 +0100

    refactor code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import PostList from "../features/posts/PostList";
+import { useGetPostsQuery } from "../features/posts/postSlice";
+import { CustomPageError } from "../ui/shared/CustomPageError";
+import { LoadingState } from "../ui/shared/LoadingState";
+import { NotFoundState } from "../ui/shared/NotFoundState";
 
 const Home = function () {
+  const { data: posts, isLoading, error } = useGetPostsQuery(null);
+
+  if (isLoading) return <LoadingState />;
+  if (error) return <CustomPageError error={error} title="Error" />;
+  if (!posts)
+    return (
+      <NotFoundState
+        title="No Post Found"
+        message="Sorry, there is no post to display, start by creating a post"
+      />
+    );
   return (
     <>
-      <PostList />
+      <PostList posts={posts} />
     </>
   );
 };
