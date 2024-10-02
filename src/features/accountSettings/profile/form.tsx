3650a68177769406c354e3a6e1214927a669946d
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-10-02 17:51:15 +0100

    feat: funtionality for managing profile
    things to expect next:
    changing profile pics,
    add more fields to user details
    enhance the form functionality of as I want the form of each individual
    dataset to be popping out once click

---
+++ src/features/accountSettings/profile/form.tsx
@@ -0,0 +1,111 @@
+import { useForm } from "react-hook-form";
+import { FormField } from "../../../ui/shared/FormField";
+import {
+  Edit2,
+  Linkedin,
+  Mail,
+  Save,
+  GithubIcon as GitHub,
+} from "lucide-react";
+import { useEffect, useState } from "react";
+import { useUpdateUserMutation } from "../../users/userSlice";
+import toast from "react-hot-toast";
+
+export const ProfileForm = ({ user, userId }) => {
+  const [updateUser, { isLoading, isSuccess, error }] = useUpdateUserMutation();
+  const { control, handleSubmit: handleSubmitProfile } = useForm({
+    defaultValues: {
+      username: user?.username || "",
+      email: user?.email,
+      bio: "Passionate about creating amazing web experiences",
+      github: "github link",
+      linkedin: "linkedin link",
+    },
+  });
+  const [isEditing, setIsEditing] = useState(false);
+
+  const onSubmitProfile = async (data: any) => {
+    if (!isEditing) {
+      return setIsEditing(true);
+    }
+    await updateUser({ formData: data, id: userId }).unwrap();
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("Update successful");
+      setIsEditing(false);
+    }
+    if (error) {
+      if ("data" in error) {
+        toast.error(error.data?.message || "An error occurred");
+      } else {
+        toast.error("An unexpected error occurred");
+      }
+    }
+  }, [isSuccess, error]);
+
+  return (
+    <form onSubmit={handleSubmitProfile(onSubmitProfile)} className="space-y-4">
+      <FormField
+        name="username"
+        control={control}
+        label="Username"
+        disabled={!isEditing}
+      />
+      <FormField
+        name="email"
+        type="email"
+        label="Email"
+        control={control}
+        icon={Mail}
+        disabled={true}
+      />
+      <FormField
+        name="bio"
+        control={control}
+        label="Bio"
+        multiline
+        disabled={!isEditing}
+      />
+      <div className="flex space-x-4">
+        <FormField
+          label="GitHub"
+          name="github"
+          control={control}
+          value="GitHub Link"
+          icon={GitHub}
+          disabled={!isEditing}
+        />
+        <FormField
+          label="LinkedIn"
+          name="linkedin"
+          control={control}
+          value="LinkedIn Link"
+          icon={Linkedin}
+          disabled={!isEditing}
+        />
+      </div>
+
+      <div className="flex justify-end space-x-4">
+        {!isEditing ? (
+          <button
+            type="submit"
+            className="bg-electricCyan-600 text-customBlue-900 px-4 py-2 rounded-lg flex items-center space-x-2"
+          >
+            <Edit2 size={18} />
+            <span>Edit</span>
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="bg-neonPink-600 text-customBlue-900 px-4 py-2 rounded-lg flex items-center space-x-2"
+          >
+            <Save size={18} />
+            <span>Save</span>
+          </button>
+        )}
+      </div>
+    </form>
+  );
+};
---
+++ src/features/accountSettings/profile/profile.tsx
@@ -0,0 +1,26 @@
+import { Author } from "../../../types/type";
+
+interface ProfileProps {
+  user: Partial<Author>;
+  children: React.ReactNode;
+}
+export const Profile = ({ user, children }: ProfileProps) => {
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center space-x-4">
+        <img
+          src={user?.photo}
+          alt="Profile"
+          className="w-24 h-24 rounded-full border-4 border-electricCyan-500"
+        />
+        <div>
+          <h2 className="text-2xl font-bold text-electricCyan-300">
+            {user?.username}
+          </h2>
+          <p className="text-neonPink-300">Full-stack Developer</p>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
