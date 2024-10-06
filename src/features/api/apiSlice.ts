0e017443fac3919bae610b5b3a952c5e6426092d
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-10-06 13:31:05 +0100

    change api endpoint to point to railway

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { UserLogin } from "../auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://blog-backend-youe.onrender.com/",
+  // baseUrl: "https://blog-backend-youe.onrender.com/",
+  baseUrl: "https://blog-backend-production-2d72.up.railway.app/",
   // baseUrl: "http://127.0.0.1:4000/",
   credentials: "include" as const,
   // prepareHeaders: (headers, { getState }: { getState: any }) => {
